697a8c1c4e1a6f0b58e586399d1b72296c20ff05
yanas-logs <yanas.dev404@gmail.com>
2026-01-06 23:17:21 +0700

    feat(about): add about page with routing

--- boilerplate/landing-page/src/app/app.routes.ts
+++ boilerplate/landing-page/src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
 
-export const routes: Routes = [{ path: '', component: Home }];
+export const routes: Routes = [
+	{ path: '', component: Home },
+	{ path: 'about', component: About },
+];
--- boilerplate/landing-page/src/app/app.ts
+++ boilerplate/landing-page/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
