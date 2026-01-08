4d966f1d04bc3a0d51fc5334298ffc57e53addfb
yanas-logs <yanas.dev404@gmail.com>
2026-01-08 23:59:44 +0700

    feat(router): add active link state and scroll restoration

--- boilerplate/landing-page/src/app/app.config.ts
+++ boilerplate/landing-page/src/app/app.config.ts
@@ -1,12 +1,9 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { routes } from './app.routes';
+
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+  providers: [provideRouter(routes), provideClientHydration(withEventReplay())],
 };
