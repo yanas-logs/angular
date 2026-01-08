63d9644b40b13b1ba34d992fec1ed088d68f1684
yanas-logs <yanas.dev404@gmail.com>
2026-01-09 00:20:37 +0700

    refactor(layout): clean semantic structure and component configs

--- boilerplate/landing-page/src/app/pages/about/about.ts
+++ boilerplate/landing-page/src/app/pages/about/about.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  standalone: true,
   templateUrl: './about.html',
-  styleUrl: './about.css',
+  styleUrls: ['./about.css'],
 })
 export class About {}
