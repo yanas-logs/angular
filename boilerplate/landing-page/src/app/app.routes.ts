554494666420ce74d57f0618382f09ad94c39ea1
yanas-logs <yanas.dev404@gmail.com>
2026-01-12 00:04:27 +0700

    feat(contact): update contact page and routing

--- boilerplate/landing-page/src/app/app.routes.ts
+++ boilerplate/landing-page/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
 
 export const routes: Routes = [
 	{ path: '', component: Home },
 	{ path: 'about', component: About },
+	{ path: 'contact', component: Contact },
 ];
