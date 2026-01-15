9916bc479d0b4c43d8318949f9431af4210879ef
yanas-logs <yanas.dev404@gmail.com>
2026-01-15 23:54:27 +0700

    refactor(navbar): add login page, features and navigation routes

--- boilerplate/landing-page/src/app/app.routes.ts
+++ boilerplate/landing-page/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Features } from './pages/features/features';
 import { About } from './pages/about/about';
 import { Contact } from './pages/contact/contact';
+import { Login } from './pages/login/login';
 
 export const routes: Routes = [
 	{ path: '', component: Home },
+	{ path: 'features', component: Features },
 	{ path: 'about', component: About },
 	{ path: 'contact', component: Contact },
+	{ path: 'login', component: Login },
 ];
--- boilerplate/landing-page/src/app/pages/features/features.ts
+++ boilerplate/landing-page/src/app/pages/features/features.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-features',
-  imports: [],
+  standalone: true,
   templateUrl: './features.html',
-  styleUrl: './features.css',
+  styleUrls: ['./features.css'],
 })
-export class Features {
-
-}
+export class Features {}
